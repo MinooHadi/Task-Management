d08b06bdb6213cc199f67234e4916bae34947769
Minoo <minoo.hadi2904@gmail.com>
2022-12-20 11:58:04 +0330

    feat: create replaceChecked function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,56 @@
 function replaceAll(from, to) {
-    const fromUl = document.querySelector(`.side-${from}`);
-    const toUl = document.querySelector(`.side-${to}`);
-    toUl.innerHTML += fromUl.innerHTML;
-    fromUl.innerHTML = "";
-    const leftAll = document.querySelector(".left-to-all");
-    const rightAll = document.querySelector(".right-to-all");
-    const leftChecked = document.querySelector(".left-to-checked");
-    const rightChecked = document.querySelector(".right-to-checked");
+  const fromUl = document.querySelector(`.side-${from}`);
+  const fromLis = fromUl.querySelectorAll("li")
+  const toUl = document.querySelector(`.side-${to}`);
+  for(let fromLi of fromLis){
+    toUl.append(fromLi);
+  }
 
-    if(from === "left"){
-        leftAll.disabled = false;
-        rightAll.disabled = true;
-        leftChecked.disabled = false;
-        rightChecked.disabled = true;
-    } else {
-        rightAll.disabled = false;
-        leftAll.disabled = true;
-        leftChecked.disabled = true;
-        rightChecked.disabled = false;
-    }
+  const leftAll = document.querySelector(".left-to-all");
+  const rightAll = document.querySelector(".right-to-all");
+  const leftChecked = document.querySelector(".left-to-checked");
+  const rightChecked = document.querySelector(".right-to-checked");
+
+  if (from === "left") {
+    leftAll.disabled = false;
+    rightAll.disabled = true;
+    leftChecked.disabled = false;
+    rightChecked.disabled = true;
+  } else {
+    rightAll.disabled = false;
+    leftAll.disabled = true;
+    leftChecked.disabled = true;
+    rightChecked.disabled = false;
+  }
 }
 
+function replaceChecked(from, to) {
+  const fromUl = document.querySelector(`.side-${from}`);
+  const fromInputs = fromUl.querySelectorAll("li input:checked");
+  const toUl = document.querySelector(`.side-${to}`);
+
+  if (!fromInputs.length) {
+    return; 
+  }
+
+  for (let fromInput of fromInputs) {
+    toUl.append(fromInput.parentElement);
+  }
+
+  const fromAll = document.querySelector(`.${to}-to-all`);
+  const fromChecked = document.querySelector(`.${to}-to-checked`);
+  const toAll = document.querySelector(`.${from}-to-all`);
+  const toChecked = document.querySelector(`.${from}-to-checked`);
+
+  if (fromUl.children.length == 0) {
+    fromAll.disabled = true;
+    fromChecked.disabled = true;
+    toAll.disabled = false;
+    toChecked.disabled = false;
+  } else {
+    fromAll.disabled = false;
+    fromChecked.disabled = false;
+    toAll.disabled = false;
+    toChecked.disabled = false;
+  }
+}
